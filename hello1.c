c6bf65fa6a9c9bcb06b6172683e04ed57c2358d8
kirAn-sAi <crazykiran289@gmail.com>
2018-05-06 13:56:48 +0530

    Create hello1.c
    It will take a name of the user and says hello to that user

---
+++ hello1.c
@@ -0,0 +1,12 @@
+//c code to print Hello with user given name
+
+#include<stdio.h>
+int main()
+{
+  char name[20];
+  pintf("Enter a name : ");
+  scanf("%s",name);
+  printf("Hello %s!",name);
+  return 0;
+}
+  
